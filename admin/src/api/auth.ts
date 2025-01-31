5a7defc88a0afd7adaae066f5a6987d0f64107cc
Hit-Bheda <hitbheda.h@gmail.com>
2025-01-31 11:34:43 +0530

    fix: Added The Dynamic API Url! Using ENV!

--- admin/src/api/auth.ts
+++ admin/src/api/auth.ts
@@ -7,9 +7,11 @@ type Res = {
         accessToken: string
     }
 }
+
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:4000"
 export const getToken = async () => {
     try{
-        const response: Res = await axios.post("http://localhost:4000/api/v1/admin/auth/access-token",null,{withCredentials:true})
+        const response: Res = await axios.post(`${BASE_URL}/api/v1/admin/auth/access-token`,null,{withCredentials:true})
         return response.data.accessToken
     }catch(error){
         console.error(error)
@@ -18,7 +20,7 @@ export const getToken = async () => {
 
 export const login = async (data:z.infer<typeof LoginSchema>) => {
     try {
-        const response = await axios.post("http://localhost:4000/api/v1/admin/auth/login",
+        const response = await axios.post(`${BASE_URL}/api/v1/admin/auth/login`,
             data,{
                 withCredentials: true
             }
@@ -35,7 +37,7 @@ export const login = async (data:z.infer<typeof LoginSchema>) => {
 
 export const logout = async () => {
     try {
-        await axios.post("http://localhost:4000/api/v1/admin/auth/logout",
+        await axios.post(`${BASE_URL}/api/v1/admin/auth/logout`,
             null,{
                 withCredentials: true
             }
@@ -53,7 +55,7 @@ export const verifyAuth = async () => {
     }
 }
     try {
-        const res:resType = await axios.post("http://localhost:4000/api/v1/admin/auth/is-auth",null,{withCredentials: true})
+        const res:resType = await axios.post(`${BASE_URL}/api/v1/admin/auth/is-auth`,null,{withCredentials: true})
         return res.data.isAuth
     } catch (error) {
         console.error(error);
